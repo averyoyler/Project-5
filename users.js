23db1b2ad047c3ed3fddb36a31b22d570d3460bf
Avery Oyler <averrryyyyyy@gmail.com>
2019-06-19 21:30:45 -0600

    add, edit, & delete user functionality

--- users.js
+++ users.js
@@ -6,9 +6,28 @@ class Users {
     this.collection.push(user);
   }
   findOne(email) {
-    for(let i = 0; i < this.users.length; i++) {
-      if(this.users[i].email === email) {
+    for(let i = 0; i < this.collection.length; i++) {
+      if(this.collection[i].email === email) {
         return this.users[i];
+      }
+    }
+  }
+  editOne(current, username, name, email, age) {
+    for(let i = 0; i < this.collection.length; i++) {
+      if(this.collection[i].name === current) {
+        this.collection[i].username = username;
+        this.collection[i].name = name;
+        this.collection[i].email = email;
+        this.collection[i].age = age;
+      }
+    }
+  }
+  deleteOne(user) {
+    for(let i = 0; i < this.collection.length; i++) {
+      if(this.collection[i].name === user) {
+        this.collection.splice(i, 1);
+        i--;
+        console.log(this.collection);
       }
     }
   }
@@ -27,8 +46,8 @@ let users = new Users();
 let avery = new User('averrryyyyyy', 'Avery Oyler', 'avery.oyler@hotmail.com', 19);
 let admin = new User('admin', 'root', 'root@mtech.org', 29);
 
-users.addOne(admin);
-users.addOne(avery);
+// users.addOne(admin);
+// users.addOne(avery);
 
 // app.post('/adduser', (req, res) => {
 //   let first = req.body.first;
